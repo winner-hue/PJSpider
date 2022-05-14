ad9be5b460d5033b0e8020612c8d2080b1ca0933
winner-hue <1344246287@qq.com>
2022-05-14 12:08:24 +0800

    update

---
+++ src/main/java/icu/fanjie/base/plugin/MysqlStorage.java
@@ -0,0 +1,14 @@
+package icu.fanjie.base.plugin;
+
+import icu.fanjie.SpiderTracker;
+import icu.fanjie.base.PrintStorage;
+
+public class MysqlStorage extends PrintStorage {
+    public MysqlStorage() {
+    }
+
+    @Override
+    public void storage(SpiderTracker tracker) {
+
+    }
+}
